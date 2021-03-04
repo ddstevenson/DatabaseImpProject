bf532fef7470d40d59c03299dc7231f8dad05b97
ddstevenson <MY_NAME@example.com>
2021-03-03 22:20:17 -0800

    Fixed 5 bugs

--- dataGen.py
+++ dataGen.py
@@ -1,27 +1,27 @@
-#dataGen.py
+# dataGen.py
 # Djernaes/Ku
 # CS487 "Scalable" Wisconsin Benchmark Implementation Project
 
-# note: need stringsGen.py as a dependency
+# need stringsGen.py as a dependency
 import numpy as np
 import pandas as pd
-import string
 from stringsGen import string_u, stringFour
 
-# generate 'maxtuples' per given input 
+
+# generate 'maxtuples' per given input
 def dataGenerator(maxtuples):
-    #Uniques
-    unique_1 = np.arange(0,maxtuples)
-    unique_3 = np.arange(0,maxtuples)
-    unique2 = np.arange(0,maxtuples) #  unique2 (id numbers 0 to size-1)
-    unique1 = np.random.permutation(unique_1) # unique1 is a random ordering of numbers from 0 to size-1
-    unique3 = np.random.permutation(unique_3) # randomized unique1
-    #Raw integers
-    two = unique1[0:] % 2
+    # Uniques
+    unique_1 = np.arange(0, maxtuples)
+    unique_3 = np.arange(0, maxtuples)
+    unique2 = np.arange(0, maxtuples)  # unique2 (id numbers 0 to size-1)
+    unique1 = np.random.permutation(unique_1)  # unique1 is a random ordering of numbers from 0 to size-1
+    unique3 = np.random.permutation(unique_3)  # randomized unique1
+    # Raw integers
+    two = unique1 % 2
     four = unique1 % 4
     ten = unique1 % 10
     twenty = unique1 % 20
-    #Percentages
+    # Percentages
     onePercent = unique1 % 100
     tenPercent = unique1 % 10
     twentyPercent = unique1 % 5
@@ -29,32 +29,35 @@ def dataGenerator(maxtuples):
     evenOnePercent = onePercent * 2
     oddOnePercent = (onePercent * 2) + 1
 
-    stringu1 = string_u(maxtuples)  # see stringsGen.py for defs
-    stringu2 = string_u(maxtuples)  # see stringsGen.py for defs
-    string4 = stringFour(maxtuples) # see stringsGen.py for defs
+    stringu1 = string_u(unique1)  # see stringsGen.py for defs
+    stringu2 = string_u(unique2)  # see stringsGen.py for defs
 
-    #assemble attributes for dataframe export
-    df = {'unique1': unique1, 'unique2': unique2, 'two': two, 'four': four, 'ten': ten, 
-        'twenty': twenty, 'onePercent': onePercent, 'tenPercent': tenPercent, 'twentyPercent': twentyPercent, 
-        'fiftyPercent': fiftyPercent, 'unique3': unique3, 'evenOnePercent': evenOnePercent, 
-        'oddOnePercent': oddOnePercent, 'stringu1': stringu1, 'stringu2': stringu2, 'string4': string4}
-    df1 = pd.DataFrame(data = df) 
+    string4 = stringFour(maxtuples)  # see stringsGen.py for defs
 
-    return df1    
+    # assemble attributes for dataframe export
+    df = {'unique1': unique1, 'unique2': unique2, 'two': two, 'four': four, 'ten': ten,
+          'twenty': twenty, 'onePercent': onePercent, 'tenPercent': tenPercent, 'twentyPercent': twentyPercent,
+          'fiftyPercent': fiftyPercent, 'unique3': unique3, 'evenOnePercent': evenOnePercent,
+          'oddOnePercent': oddOnePercent, 'stringu1': stringu1, 'stringu2': stringu2, 'string4': string4}
+    df1 = pd.DataFrame(data=df)
+
+    return df1
 
 
-"""  main()  """ 
+"""  main()  """
 # NOTE: maxtuples must be divisible by 4, otherwise program 
 # will error. This is due to string4's attribute structure.
 # 10K, 100K, 1M will all work. 
 maxtuples = 1000
-finaltup = dataGenerator(maxtuples) 
+finaltup = dataGenerator(maxtuples)
 
 """ test output, print to screen """
-#print(testTup)
+# print(testTup)
 
 # export to CSV file
 # Rename output file with desired title 
 """ uncomment the next line to output to file """
-finaltup.to_csv('tenktup.csv', index = True)
+# finaltup.to_csv('testTuples.csv')
+
+finaltup.to_csv('tenmiltup.csv')
 print("file complete")
--- stringsGen.py
+++ stringsGen.py
@@ -1,48 +1,52 @@
-#stringsGen
+# stringsGen
 import numpy as np
-import pandas as pd
-import string
-import random
 
-# part of the StringuX attributes. The string attribute is completed in 
-# two steps. first generate the random part of the string (randomString())
-# then tack on the string of Xs to the end (fullString()). 
-def randomString(stringLength = 7):
+
+# part of the StringuX attributes.
+def convert_to_string(unique):
     # generate a string of a fixed length
-    letters = string.ascii_uppercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
+    result = ["A"] * 7
+    i = 6
+    cnt = 0
+    tmp = [" "] * 7
+    # convert unique value from right to left into an alphabetic string in result
+    while unique > 0:
+        rem = unique % 26
+        tmp[i] = chr(ord('A') + rem)
+        unique = unique // 26
+        i -= 1
+        cnt += 1
 
-def fullString():
-    # append 45 Xs to the string. Split into to 
-    trailingX ="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # 45 Xs
-    return "{}{}".format(randomString(7),trailingX)             # concatenate output
+    # This could be implemented more elegantly in python, but where's the fun in that?
+    # Not certain this loop is necessary, since lots of student-written scripts don't include it...
+    for j in range(cnt + 1):
+        result[j] = tmp[i]
+        i += 1
+
+    return ''.join(result) + ("x" * 45)
 
 
 # generate strings attributes stringu1 and stringu2
-def string_u(maxtuples):            
-    stringu = np.chararray(0)           # allocate char array
-    stringu = stringu.astype('U52')     # set size to allow for 52 char string
-    for i in range(maxtuples):          # fill the array with the random strings 
-        stringu = np.append(stringu, fullString())  # to size maxtuples
+def string_u(unique_arr):
+    stringu = np.chararray(0)  # allocate char array
+    stringu = stringu.astype('U52')  # set size to allow for 52 char string
+    for i in range(len(unique_arr)):  # fill the array with the random strings
+        stringu = np.append(stringu, convert_to_string(unique_arr[i]))  # to size maxtuples
 
     return stringu
 
+
 def stringFour(maxtuples):
-    #STRING4 DATA
+    # STRING4 DATA
     string4 = np.chararray(0)
     string4 = string4.astype('U52')
-    #create a matching sized set of TENKTUP by appending the cycle of four, 
+    # create a matching sized set of TENKTUP by appending the cycle of four,
     # maxtuples must be divisible by 4 or the output will error, since the
     # arrays will not be of equal lengths.  
-    for i in range(maxtuples//4):
-        string4 = np.append(string4,"AAAAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-        string4 = np.append(string4,"HHHHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-        string4 = np.append(string4,"OOOOXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-        string4 = np.append(string4,"VVVVXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    for i in range(maxtuples // 4):
+        string4 = np.append(string4, "AAAA" + ('x' * 48))
+        string4 = np.append(string4, "HHHH" + ('x' * 48))
+        string4 = np.append(string4, "OOOO" + ('x' * 48))
+        string4 = np.append(string4, "VVVV" + ('x' * 48))
     return string4
 
-#convert to pandas dataframe for ease of CSV file creation
-#string4 = pd.DataFrame(string4)
-#string4.to_csv('string_4.csv')
-
-#print(string_u(8))
